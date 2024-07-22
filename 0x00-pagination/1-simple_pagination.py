66d1ec67d913e4d5d5844edaa79e20a7d50468ce
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-22 12:09:32 +0000

    g gg

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,7 +1,13 @@
 import csv
 import math
-from typing import List
+from typing import List, Tuple
 
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''' index range'''
+    start_idx = (page - 1) * page_size
+    end_idx = start_idx + page_size
+    return (start_idx, end_idx)
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -23,4 +29,10 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            pass
+           assert type(page) == int and type(page_size) == int 
+           assert page > 0 and page_size > 0
+           start_idx, end_idx = index_range(page, page_size)
+           data = self.dataset()
+           if len(data) < start_idx:
+               return []
+           return data[start_idx:end_idx]
