073cfd54edfee474243813f59e02b26383605435
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-22 13:27:47 +0000

    dp

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+'''simple pagination task1'''
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''' index range'''
+    start_idx = (page - 1) * page_size
+    end_idx = start_idx + page_size
+    return (start_idx, end_idx)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start_idx, end_idx = index_range(page, page_size)
+        data = self.dataset()
+        if start_idx > len(data):
+            return []
+        return data[start_idx:end_idx]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        '''get all information about a page'''
+        pageData = self.get_page(page, page_size)
+        start_idx, end_idx = index_range(page, page_size)
+        pageTotal = math.ceil(len(self.__dataset) / page_size)
+        info = {
+                'page_size': len(pageData),
+                'page': page,
+                'data': pageData,
+                'next_page': page + 1 if end_idx < len(self.__dataset) else None,
+                'prec_page': page - 1 if start_idx > 0 else None,
+                'total_pages': pageTotal
+                }
+        return info
