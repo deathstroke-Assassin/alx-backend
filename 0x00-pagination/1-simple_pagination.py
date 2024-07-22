af2eac4edf54ccdddd0f7a94b963925d1e5342ea
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-22 12:26:24 +0000

    p3ew

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -32,6 +32,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data.
+        """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start_idx, end_idx = index_range(page, page_size)
