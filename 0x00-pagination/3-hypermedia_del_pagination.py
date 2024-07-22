a9c73159609623ec63e9a851d92a32a9437e73c0
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-22 14:22:33 +0000

    gghgh

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -3,15 +3,7 @@
 Deletion-resilient hypermedia pagination
 """
 import csv
-import math
-from typing import List, Tuple, Dict
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    '''' index range'''
-    start_idx = (page - 1) * page_size
-    end_idx = start_idx + page_size
-    return (start_idx, end_idx)
+from typing import Dict, List
 
 
 class Server:
@@ -20,7 +12,10 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
+        self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
         """Cached dataset
@@ -44,32 +39,6 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
-        """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
-        start_idx, end_idx = index_range(page, page_size)
-        data = self.dataset()
-        if start_idx > len(data):
-            return []
-        return data[start_idx:end_idx]
-
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        '''get all information about a page'''
-        pageData = self.get_page(page, page_size)
-        start_idx, end_idx = index_range(page, page_size)
-        pageTotal = math.ceil(len(self.__dataset) / page_size)
-        info = {
-            'page_size': len(pageData),
-            'page': page,
-            'data': pageData,
-            'next_page': page + 1 if end_idx < len(self.__dataset) else None,
-            'prev_page': page - 1 if start_idx > 0 else None,
-            'total_pages': pageTotal,
-                }
-        return info
-
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         '''hyper deletion resilient pagination'''
         data = self.indexed_dataset()
@@ -80,7 +49,7 @@ class Server:
         startIndex = index if index else 0
         for k, value in data.items():
             if k >= startIndex and dataCount < page_size:
-                pageData.append(item)
+                pageData.append(value)
                 dataCount += 1
                 continue
             if dataCount == page_size:
@@ -92,4 +61,4 @@ class Server:
             'page_size': len(pageData),
             'data': pageData,
         }
-        return indexInfo
+        return indexinfo
